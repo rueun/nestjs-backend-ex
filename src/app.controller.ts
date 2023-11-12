3d662c471f67d4a9773db3f81b56e5329b1f6367
ejshin <ejshin@muhayu.com>
2023-11-12 22:54:12 +0900

    feat: 요청 객체 테스트 (@Query, @Param 등)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { query } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/hello')
   getHello(): string {
     return this.appService.getHello();
   }
+  
+  // @Req() 데코레이터는 Request 객체를 가져온다.
+  @Get()
+  getHello2(@Req() req: Request): string {
+    console.log(req);
+    return this.appService.getHello();
+  }
+
+  // @query() 데코레이터는 쿼리 스트링을 가져온다.
+  @Get('/name')
+  getName(@Query() name: string): string {
+    return name;
+  }
+
+  // @Param() 데코레이터는 URL 파라미터를 가져온다.
+  @Get('key/:key')
+  getKey(@Param() key: string): string {
+    return key;
+  }
+
+
 }
