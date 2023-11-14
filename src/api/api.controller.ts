bfabb981b96ead9d1282aa3be1015cfae9afcbf0
rueun <dkfah1229@naver.com>
2023-11-15 00:05:31 +0900

    feat: 하위 도메인 라우팅 예제 추가

--- src/api/api.controller.ts
+++ src/api/api.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
-@Controller('api')
-export class ApiController {}
+@Controller({host: 'api.localhost'}) // 하위 도메인 요청 처리 설정
+export class ApiController {
+
+    @Get() // 같은 루트 경로
+    index(): string {
+        return 'Hello Api'; // 다른 응답
+    }
+}
