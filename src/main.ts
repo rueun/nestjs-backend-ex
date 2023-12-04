7c1625e00e7058dc0fb48bc183ab717a01b2bce2
신은정 <ejshin@muhayu.com>
2023-12-04 09:26:37 +0900

    #feat : 스웨거 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+const setupSwagger = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('My Project')
+    .setDescription('My Project API')
+    .setVersion('0.0.1') // 이거 실제 플젝에서는 `package.json` 에서 가져올거임
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
   await app.listen(3000);
 }
 bootstrap();
